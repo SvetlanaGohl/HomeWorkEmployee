846191b843d9f3e5d12284b411566ad2c6e7612c
Svetlana Gohl <sm181183@mail.ru>
2023-09-13 23:23:36 +0300

    Начальный коммит

---
+++ src/test/java/skypro/employeeBook/gohl/EmployeeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package skypro.employeeBook.gohl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
